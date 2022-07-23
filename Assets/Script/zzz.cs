b6496088f0b12f1bad4b750d8b79da5a34fd1c62
ungan <83686779+ungan@users.noreply.github.com>
2022-07-23 18:52:03 +0900

    a2
    a

--- Assets/Script/zzz.cs
+++ Assets/Script/zzz.cs
@@ -6,11 +6,11 @@ public class zzz : MonoBehaviour
 {
     public int hp;
     public int cur_hp;
-    
+    public int ppp2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ppp2 = 122;
     }
 
     // Update is called once per frame
